729df04e3027228cac9173c635455b7c3ff423dd
TomasJurk <tomasstj@gmail.com>
2018-04-27 14:52:51 +0300

    login functional done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -22,6 +23,8 @@ import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './admin/login/login.component';
 import { PostsComponent } from './admin/posts/posts.component';
 import { RegistrationsComponent } from './admin/registrations/registrations.component';
+
+import { AuthService } from './core/auth.service';
 
 
 @NgModule({
@@ -44,9 +47,13 @@ import { RegistrationsComponent } from './admin/registrations/registrations.comp
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AppRoutingModule
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
