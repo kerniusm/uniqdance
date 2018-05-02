8467fabd1b02d3d130a43b8170097a02c780231f
TomasJurk <tomasstj@gmail.com>
2018-05-02 13:20:59 +0300

    Registrations at admin panel done

--- src/app/core/registrations.service.ts
+++ src/app/core/registrations.service.ts
@@ -23,13 +23,25 @@ export class RegistrationsService {
     return this.afs.collection('registrations').add(registration);
   }
 
+  deleteRegistration(id) {
+    return this.afs.doc<any>(`registrations/${id}`).delete();
+  }
+
   getAllRegistrations() {
-    return this.afs.collection('registrations', ref => ref.orderBy('posted_on', 'desc')).snapshotChanges()
+    return this.afs.collection('registrations', ref => ref.orderBy('registrated_on', 'desc')).snapshotChanges()
     .map(result => {
       return result.map(resu => {
         const data = resu.payload.doc.data();
         return {
-          //
+          registrationID: resu.payload.doc.id,
+          name: data.name,
+          surname: data.surname,
+          city: data.city,
+          age: data.age,
+          phone: data.phone,
+          time: data.time,
+          email: data.email,
+          message: data.message
         };
       });
     });
